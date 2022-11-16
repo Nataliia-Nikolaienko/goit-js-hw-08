3abf1255064555463a584514bc7c37a55bb7ce58
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2022-11-16 14:47:45 +0200

    changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
@@ -16,11 +16,11 @@ function createGalleryMarkup(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
             return `
-        <li class="gallery__item">
+        <ul><li class="gallery__item">
   <a class="gallery__link" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>
-</li>
+</li></ul>
 `
         })
         .join('');
