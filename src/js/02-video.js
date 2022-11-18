03b956c47705757d20f219dbb46e33b99683dc42
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2022-11-18 20:18:15 +0200

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(setWatchingTime, 500));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+function setWatchingTime(event) {
+    localStorage.setItem("videoplayer-current-time", event.seconds);
+};
+
 
