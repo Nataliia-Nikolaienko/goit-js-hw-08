2d19c4a4f21d27cc6333526012ff580a47bb5ae7
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2022-11-19 15:37:48 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+};
+
+function onTextareaInput(evt) {
+    const message = evt.currentTarget.value;
+
+    localStorage.setItem('feedback-form-state', message);
+};
